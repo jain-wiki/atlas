28dbbe2a82f247c2aa989843dd9416f49bf41b3c
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-07-28 02:00:37 +0530

    added config for auth

--- apps/backend/lib/auth.ts
+++ apps/backend/lib/auth.ts
@@ -1,16 +1,50 @@
-import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
+import { betterAuth } from 'better-auth';
+import { oneTap } from 'better-auth/plugins';
+import Database from 'better-sqlite3';
 
 export const auth = betterAuth({
-  database: new Database("./db.sqlite"),
+  appName: 'Jain Atlas',
+  database: new Database('./db.sqlite'),
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ['google']
+    }
+  },
+  advanced: {
+    cookiePrefix: 'atlas',
+  },
+  session: {
+    // Ref: https://www.better-auth.com/docs/concepts/session-management#cookie-cache
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60 // Cache duration in seconds. i.e 5 minutes
+    }
+  },
+  rateLimit: {
+    // NOTE: since we did not specify the database, it will use the default in-memory store.
+    enabled: true,
+    window: 10, // time window in seconds
+    max: 20, // max requests in the window
+  },
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+    minPasswordLength: 8, // Minimum password length
+    maxPasswordLength: 24, // Maximum password length
+    revokeSessionsOnPasswordReset: true,
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      // Optionally, you can add prompt/accessType for refresh tokens:
-      // prompt: "select_account+consent",
-      // accessType: "offline",
     },
   },
-  // Session management and other options can be added here if needed
+  plugins: [
+    oneTap(), // Add the One Tap server plugin
+  ]
 });
+
+
+export type Session = typeof auth.$Infer.Session.session;
+export type User = typeof auth.$Infer.Session.user;
