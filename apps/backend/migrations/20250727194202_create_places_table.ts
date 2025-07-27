bbf29b6e868f9e977d6bac870c4501129d9f3ada
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-07-28 02:26:33 +0530

    refactor: consolidate places and places_log table creation into a single migration

--- apps/backend/migrations/20250727194202_create_places_table.ts
+++ apps/backend/migrations/20250727194202_create_places_table.ts
@@ -1,6 +1,4 @@
-import type { Knex } from "knex";
-
-
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('places', (table) => {
@@ -12,10 +10,22 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
+
+  await knex.schema.createTable('places_log', (table) => {
+    table.increments('id').primary();
+    table.string('place_id', 10).references('id').inTable('places');
+    table.string('action', 1); // (I, U, D)
+    table.text('old_data'); // JSON
+    table.text('new_data'); // JSON
+    table.string('user_email', 255);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTableIfExists('places');
+  await knex.schema.dropTableIfExists('places_log');
+
 }
 
