35f8ec9f72e68e6f17496976ae959f8635f024cb
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-07-29 18:25:10 +0530

    refactor: remove Knex migrations and initialize database schema directly from SQL file

--- apps/backend/lib/db.ts
+++ apps/backend/lib/db.ts
@@ -1,8 +1,6 @@
 // ref: https://bun.sh/docs/api/sqlite.md
 
 import { Database } from 'bun:sqlite';
-import fs from 'fs';
-import path from 'path';
 
 export const db = new Database('db.sqlite', {
   create: true,
@@ -13,12 +11,6 @@ db.exec('VACUUM;'); // Compact the database file when the application starts
 db.exec('PRAGMA foreign_keys = ON;'); // Enable foreign key constraints
 db.exec('PRAGMA journal_mode = WAL;'); // Enable Write-Ahead Logging for better concurrency
 
-// Initialize database schema
-const schemaPath = path.join(import.meta.dir, 'schema.sql');
-if (fs.existsSync(schemaPath)) {
-  const schema = fs.readFileSync(schemaPath, 'utf8');
-  db.exec(schema);
-}
 
 // Print sqlite version
 const version = db.query('SELECT sqlite_version() AS version;').get();
@@ -34,3 +26,13 @@ process.on('SIGINT', () => {
   db.close();
   process.exit(0);
 });
+
+
+// Initialize database schema, if this file is run directly
+// This is useful for development and testing purposes
+// @ts-ignore
+import sqlText from './schema.sql' with { type: 'text' };
+if (import.meta.main) {
+  db.exec(sqlText);
+  console.log('Database initialized and ready to use. Ensure that the db.sqlite file is moved to the ``apps/backend/lib/`` directory.');
+}
