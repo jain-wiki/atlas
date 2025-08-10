7dc5daa329d6484a0eff5dd35e9c33f7bbd88042
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-08-10 14:42:41 +0530

    added type for Plac

---
+++ packages/types/src/gplace.ts
@@ -0,0 +1,13 @@
+export interface Place {
+  id: string
+  rtree_id: number
+  displayName: string
+  administrativeArea?: string
+  locality?: string
+  pincode?: string
+  digipin5: string
+  response: string
+  classification?: string
+  item?: string
+  createdAt: string
+}
