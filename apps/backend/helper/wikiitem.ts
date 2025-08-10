1b9911a4178321bdc40e43fa5900aee38420a71b
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2025-08-10 16:28:44 +0530

    added createWikiItem()

---
+++ apps/backend/helper/wikiitem.ts
@@ -0,0 +1,31 @@
+// Ref: https://github.com/maxlath/wikibase-edit/blob/main/docs/how_to.md
+
+// @ts-ignore
+import WBEdit from 'wikibase-edit'
+
+const JAIN_WIKI_API_URL = process.env.JAIN_WIKI_API_URL
+const JAIN_WIKI_USERNAME = process.env.JAIN_WIKI_USERNAME
+const JAIN_WIKI_PASSWORD = process.env.JAIN_WIKI_PASSWORD
+
+if (!JAIN_WIKI_API_URL || !JAIN_WIKI_USERNAME || !JAIN_WIKI_PASSWORD) {
+  throw new Error('Missing JAIN_WIKI environment variables')
+}
+
+const wbEdit = WBEdit({
+  instance: JAIN_WIKI_API_URL,
+  credentials: {
+    username: JAIN_WIKI_USERNAME,
+    password: JAIN_WIKI_PASSWORD,
+  },
+  tags: ['jain-atlas']
+})
+
+export async function createWikiItem(title: string, description: string, claims: Record<string, string>) {
+  const { entity } = await wbEdit.entity.create({
+    type: 'item',
+    labels: { 'en': title },
+    descriptions: { 'en': description },
+    claims
+  })
+  return entity.id
+}
